7fb88d2d2568ca6b0cfc858c2fbb88737f7676cb
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-16 14:09:10 +0200

    create task

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,14 @@
-console.log("test");
+/*
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+// TASK
+//visualizzare 5 numeri random (potenzialmente con un bottone)
+//creare 5 elementi html (un UL) per i numeri
+//creare un bottone con cui attivare il tutto
+//attivare un timer di 30 secondi che toglie i numeri
+//allo scadere del timer appaiono 5 input per inserire i numeri
+//creare un bottone per inviare il risultato
