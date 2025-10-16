0827e5df2f7c72b8972207eb763cce9cc6eee6e8
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-16 14:49:50 +0200

    change html element

--- assets/main.js
+++ assets/main.js
@@ -12,3 +12,41 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 //attivare un timer di 30 secondi che toglie i numeri
 //allo scadere del timer appaiono 5 input per inserire i numeri
 //creare un bottone per inviare il risultato
+
+const timerEl = document.getElementById("timer");
+console.log(timerEl);
+
+const randomEl = document.querySelector("#random_list");
+
+const pc_number = [0];
+
+for (let i = 0; i < 5; i++) {
+  const numero_casuale = Math.floor(Math.random() * 100) + 1;
+  pc_number.push(numero_casuale);
+  randomEl.appendChild(li);
+  console.log(pc_number);
+}
+
+/*
+
+let countdown = 30;
+console.log(countdown);
+
+
+timerEl.innerHTML = countdown;
+
+const clock = setInterval(() => {
+
+
+  if (countdown === 0) {
+    clearInterval(clock);
+    console.log("fermare il countdown");
+
+    timerEl.innerHTML = "auguriiii";
+  } else {
+    timerEl.innerHTML = countdown;
+    console.log("mostro il valore che scende");
+  }
+  countdown--;
+},);
+ */
